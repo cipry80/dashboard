d0170e02338e725af6cf9a8773645a56c7343e3b
ciprian.vacaru <ciprian.vacaru@fortech.ro>
2021-03-18 08:34:21 +0200

    fix my card style and layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 import { ToggleButtons, CardGroup, MyCard, Card, Tooltip, CircularProgressBar } from '../components';
 import { isMobileDevice } from '../helpers/isMobileDevice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle, faStar, faPlayCircle, faClock, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import {
+    faUserCircle,
+    faStar,
+    faPlayCircle,
+    faClock,
+    faChevronRight,
+    faPencilAlt
+} from '@fortawesome/free-solid-svg-icons';
 
 import analytics from '../images/analytics.png';
 import statistics from '../images/statistics.png';
@@ -92,7 +99,8 @@ const Home: React.FunctionComponent = () => {
             </div>
             {(checked || !isMobileDevice) && (
                 <MyCard>
-                    <h3>Total Sales</h3>
+                    {!isMobileDevice && <FontAwesomeIcon icon={faPencilAlt} />}
+                    {isMobileDevice && <h3>Total Sales</h3>}
                     <span className="badge-total-sales">$10,600</span>
                     <CircularProgressBar />
                 </MyCard>
